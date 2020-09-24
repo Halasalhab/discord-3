a3263d2244343475ddde98162ceaf0f8640b2a9b
Halasalhab <halasalhab0@gmail.com>
2020-09-24 10:12:16 +0300

    hope this will work

---
+++ discord.js
@@ -0,0 +1,7 @@
+var regis=document.getElementsByClassName('r')
+regis.click(function(){
+	window.open('https://discord.com/register')
+})
+$('.maindiv.inp').click(function(){
+ 	inp.css='borderBottom,#753FD8';
+ });
